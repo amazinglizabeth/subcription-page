3cf2add72af2d10fe4e38570f94583b660b85073
amazing lizabeth <eadenike14@gmail.com>
2024-07-27 21:55:19 +0100

    dark mode version

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 "use strict";
+const modeToggle = document.getElementById("modeToggle");
+
+modeToggle.onclick = function () {
+  document.body.classList.toggle("dark-theme");
+
+  // Change the text of the button
+  if (document.body.classList.contains("dark-theme")) {
+    modeToggle.innerHTML =
+      '<img src="images/moon.png" alt="button" />Switch to light mode';
+  } else {
+    modeToggle.innerHTML =
+      '<img src="images/sun.png" alt="button" />Switch to dark mode';
+  }
+};
+
 document.getElementById("toggleButton").addEventListener("click", function () {
   var displayText = document.getElementById("displayText");
   var inputFields = document.querySelectorAll("input");
@@ -23,7 +38,7 @@ document.getElementById("toggleButton").addEventListener("click", function () {
   if (allFilled) {
     displayText.classList.toggle("hidden");
     displayText.classList.toggle("visible");
-    alert("Your monthly newsletter subscription is succesful.");
+    alert("Your monthly newsletter subscription is successful.");
   } else {
     alert("Please fill in all required information");
   }
